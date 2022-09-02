eb38e30dd72030486e3f0b756b5311a7530a7a8a
Carlyle Komalram <glaw14@gmail.com>
2022-09-01 20:27:51 -0500

    changing type 1.1.1

--- src/index.js
+++ src/index.js
@@ -16,4 +16,12 @@ const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
 };
-module.exports = { funnyCommit };
+
+/**
+ * Name to test this function.
+ * @param {string} name 
+ */
+const helloWorld = (name) => {
+  console.log(`Hola ${name}.`);
+}
+module.exports = { funnyCommit ,helloWorld };
